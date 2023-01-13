188fdd53ba6ee46f70eda8aa7db80f83fae0b5a9
Luis Mariano Lopez lena <luismllv89@gmail.com>
2023-01-13 15:16:07 -0600

    Journal Page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,36 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      
+      {/* <Typography>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae repellat fugit placeat.
+        Doloribus necessitatibus neque quos labore aspernatur reiciendis nemo saepe architecto odit! Fugit,
+        laudantium dicta numquam assumenda nisi similique.
+      </Typography> */}
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
+
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor:'error.main',
+          ':hover': {backgroundColor:'error.main',opacity:0.9},
+          position:'fixed',
+          right:50,
+          bottom:50
+        }}
+
+      >
+      <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
+      
+    </JournalLayout>
   )
 }
